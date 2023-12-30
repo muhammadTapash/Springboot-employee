172a1c177307fe5894e4735a8694e376452514b8
muhammadtapash <tap07za@yahoo.com>
2023-12-30 10:45:59 -0500

    Add existing project files to Git

---
+++ src/main/java/cogent/infotech/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package cogent.infotech.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import cogent.infotech.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
